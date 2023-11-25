1d05b4ed157d474aac0cff6914d48d4cce673627
unknown <layalilinda4@gmail.com>
2023-11-25 10:41:55 +0700

    crud sederhana

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang= "en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" contect="IE=edge">
+        <meta name="viewport" contect="width=device-width, intial-scale=1.0">
+        <tittle>Document</title>
+</head>
+<body>
+    <h1>Sistem Informasi Sekolah</h1>
+    <h2>SMK Unipdu</h2>
+    <a href="data_siswa.php">Daftar Siswa</a><br>
+    <a href="tambah_siswa.php">Pendaftaran</a>
+</body>
+</html>
